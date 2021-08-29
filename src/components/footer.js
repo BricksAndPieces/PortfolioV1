dc44230d01d84193d57857b9dddf678dbaf2b310
IshanShetty <ishan.h.shetty@gmail.com>
2021-08-29 16:46:33 -0500

    easter egg hint in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,12 @@ import styles from './footer.module.css'
 class Footer extends Component {
 	render() {
 		return (
-			<p>Footer</p>
+			<>
+				<p>
+					Have you tried clicking on the <a href={"#moon"}><b>Moon</b></a>?
+				</p>
+				<p>Made with ❤️ by Ishan Shetty</p>
+			</>
 		);
 	}
 }
